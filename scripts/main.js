2ffae37a80d7dec96d8594531a93e765aea5b25e
Grzegorz Dubiel <greg2012201@gmail.com>
2020-09-29 17:10:17 +0200

    main.js style.css transition added

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
 const feed = () => {
 
-    // createAndAddElement();
+    // createAndAddElement() w niej animation animation;
+    // 
     const li = document.createElement('li');
+    li.classList.add('disActive')
     li.textContent = '00:00'
     document.querySelector('ul').appendChild(li);
 
 
+    setTimeout(() => {
+            li.classList.add('active');
+        },
+        10); // przenieść to trzeba na osobne zgadnienie związane z dodawaniem li i animacją
 
 }
 
