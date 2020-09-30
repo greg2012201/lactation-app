3fd484ca4a4925d0d5ec4243bb8ee16e1c2b9deb
Grzegorz Dubiel <greg2012201@gmail.com>
2020-09-30 21:37:11 +0200

    countingDown updated in counter.js

--- scripts/Counter.js
+++ scripts/Counter.js
@@ -1,25 +1,37 @@
 class Counter {
+    constructor() {
 
+    }
 
     render() {
-        let second = 0;
-        let minute = 0;
-        let hour = 0;
 
-        this.countingDown(minute, hour, second);
+
+        this.countingDown();
 
 
     }
 
-    countingDown(minute, hour, second) {
-        let time = 1000;
-        if (this.indexInterval) this.stopAndReset();;
+    countingDown() {
+        let time = 100;
+        let seconds = 60;
+        let minutes = 60;
+        let hours = 2;
+        if (this.indexInterval) this.stopAndReset();
         // zastanowić się czy nie dodać tego jako osobną metodę w tej klasie
         this.indexInterval = setInterval(() => {
-            second += 1;
-            console.log(second);
-            minute += 1;
-            hour += 1;
+            seconds -= 1;
+            if (seconds == 1) seconds = 61;
+            else if (seconds == 60) {
+                minutes == 0;
+                minutes -= 1
+            } else if (minutes == 0) {
+                hours -= 1;
+            }
+
+
+            console.log(seconds, minutes, hours);
+
+
 
         }, time);
 
