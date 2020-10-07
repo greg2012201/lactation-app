54e4e09990c5a8be3d868ab22786f4edb1782f72
Grzegorz Dubiel <greg2012201@gmail.com>
2020-10-07 17:40:58 +0200

    bugs of count down fixed

--- scripts/Counter.js
+++ scripts/Counter.js
@@ -1,5 +1,5 @@
 class Counter { // zastanawiam się czy lista nie powinna rozszerza countera
-    constructor() {
+    constructor( /* getNowTime */ /* zrobić tak ? */ ) {
         this.secondsSpan = document.querySelector('.seconds');
         this.minutesSpan = document.querySelector('.minutes');
         this.hoursSpan = document.querySelector('.hours');
@@ -10,14 +10,13 @@ class Counter { // zastanawiam się czy lista nie powinna rozszerza countera
 
         this.countingDown(this.getEndTime());
         // tutaj normalnie dopalić nowy licznik 
+
     }
 
     countingDown(endTime) {
 
         const time = 1000;
-
-
-        if (this.indexInterval) this.stopAndReset(); //<-- tutaj użyć ternery operator zamiast robić if else 
+        if (this.indexInterval) this.stopAndReset();
         this.indexInterval = setInterval(() => {
             this.getDifferenceBetweenDates(this.getNowTime(), endTime);
             this.print();
@@ -26,6 +25,13 @@ class Counter { // zastanawiam się czy lista nie powinna rozszerza countera
         }, time);
 
 
+
+    }
+    getDifferenceBetweenDates(nowTime, endTime) {
+        this.hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24); // zmienić nazwy zmiennych na np. secondsToEnd
+        this.minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+        this.seconds = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+        return this.seconds
 
     }
 
@@ -37,14 +43,9 @@ class Counter { // zastanawiam się czy lista nie powinna rozszerza countera
 
         this.seconds <= 9 ? this.secondsSpan.textContent = `0${this.seconds}` : this.secondsSpan.textContent = this.seconds;
 
-
-
         this.minutes <= 9 ? this.minutesSpan.textContent = `0${this.minutes}` : this.minutesSpan.textContent = this.minutes;
 
         this.hours <= 9 ? this.hoursSpan.textContent = `0${this.hours}` : this.hoursSpan.textContent = this.hours;
-
-        /* if (seconds == 60) this.secondsSpan.textContent = '00'
-        else if (minutes == 60) this.minutesSpan.textContent = '00'; */
 
         // trzeba do bardziej zoptymalizować stworzyć może jakąś metodę co robi to wszystko na raz żeby kodu nie kopiować i zaobaczyć czy wtedy będzie bardziej optymalnie
 
@@ -53,47 +54,32 @@ class Counter { // zastanawiam się czy lista nie powinna rozszerza countera
     stopAndReset() {
 
         clearInterval(this.indexInterval);
-        /* seconds = 60;
-        minutes = 60;
-        hours = 60; */
+        this.seconds = 0;
+        this.minutes = 0;
+        this.hours = 2;
+        this.print();
 
-        this.secondsSpan.textContent = '00';
-        this.minutesSpan.textContent = '00';
-        this.hoursSpan.textContent = '02'; // można określić w obiekcie sztywno co jest automatycznie printowane
+        /*    this.secondsSpan.textContent = this.seconds;
+           this.minutesSpan.textContent = this.minutes;
+           this.hoursSpan.textContent = this.hours; */ // można określić w obiekcie sztywno co jest automatycznie printowane
 
 
     }
 
     getEndTime() { // tutaj trzeba wyodrębnić dokładnie godzinę stopu
 
-
-        const date = new Date(); // przypisać to jako funkcje prywatne dla metody ? 
+        const date = new Date();
         let day = date.getDate();
-        day = day < 10 ? `0${day}` : day; // to może robić inna metoda tutaj i trzeba nad tym pomyśleć - powinna ona zwracać to co przerobi
         const year = date.getFullYear();
         let month = date.getMonth() + 1;
-        month = month < 10 ? `0${month}` : month;
         let hour = date.getHours();
-        hour = hour < 10 ? `0${hour}` : hour;
         let minute = date.getMinutes();
-        minute = minute < 10 ? `0${minute}` : minute;
         let second = date.getSeconds();
-        second = second < 10 ? `0${second}` : second;
-        console.log(second);
-        // dodawanie 0 powinno się odbywać raczej w renderze 
-
 
         const endTime = new Date(`${year}-${month}-${day} ${hour +2}:${minute}:${second +1}`).getTime();
-        console.log(`${year}-${month}-${day} ${hour +2}:${minute}:${second}`);
+        console.log(`${year}-${month}-${day} ${hour +2}:${minute}:${second +1}`);
 
         return endTime
-
-
-
-
-
-
-
 
     }
 
@@ -101,20 +87,6 @@ class Counter { // zastanawiam się czy lista nie powinna rozszerza countera
         return new Date().getTime();
     } // dac do do maina ? 
 
-
-
-    getDifferenceBetweenDates(nowTime, endTime) {
-
-
-
-        this.hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24); // zmienić nazwy zmiennych na np. secondsToEnd
-        this.minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
-        this.seconds = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
-        // zretunrować to do printa 
-        console.log(this.seconds);
-        return this.seconds
-
-    }
 
 
 }
