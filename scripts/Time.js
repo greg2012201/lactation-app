54e316185f412fc85e0c7664bec065e5b755ebab
Grzegorz Dubiel <greg2012201@gmail.com>
2020-10-13 16:56:25 +0200

    style.css and Time.js updated

--- scripts/Time.js
+++ scripts/Time.js
@@ -17,19 +17,6 @@ class Time { // to wyleci najprawdopodobniej do countera bo już jest to martwy 
         const date = this.initNewDate().addHours(2, 1);
 
 
-
-
-        /*  date.addHours(2, 1);  */
-
-
-        /*  const day = this.initNewDate().getDate();
-         const year = this.initNewDate().getFullYear();
-         const month = this.initNewDate().getMonth() + 1; // nie wiem czy z month nie będzie tego samego problemu co niegdyś z godziną może można to załatwić warunkiem który mówi że jeśli miesiąc jest 13 to ustawia się go na 1 + 1 tylko czy wetedy nam rok przeskoczy ? 
-         const hours = this.getFormatedTime(this.initNewDate().addHours(2, 1))[0]; // cyferki do bani bo się mapyb robią 
-         const minutes = this.getFormatedTime(this.initNewDate())[1];
-         const seconds = this.getFormatedTime(this.initNewDate())[2]; */
-
-
         const year = date.getFullYear();
         const month = date.getMonth() + 1;
         const day = date.getDate();
@@ -43,7 +30,7 @@ class Time { // to wyleci najprawdopodobniej do countera bo już jest to martwy 
 
         /* const endTime = new Date(date.toLocaleString()).getTime(); */ // do wywalenia bo się rozjeżdża względem innych 
 
-        /* const endTime = new Date('2020-10-13 23:00').getTime(); */
+        /* const endTime = new Date('2020-10-12 16:00').getTime(); */
         /* const endTime = date.toLocaleString() */
 
         return endTime;
@@ -52,9 +39,9 @@ class Time { // to wyleci najprawdopodobniej do countera bo już jest to martwy 
     }
 
 
-    getLocalTime(addUnits) { // tutaj zmiana nazwy tej metody 
+    getLocalTime(numberOfHoursAdded) { // tutaj zmiana nazwy tej metody 
         const date = this.initNewDate();
-        if (addUnits) date.addHours(addUnits, 0);
+        if (numberOfHoursAdded) date.addHours(numberOfHoursAdded, 0);
 
         // powinno się odwoływać to this.date z konstruktora
         const hour = date.getHours();
@@ -73,27 +60,7 @@ class Time { // to wyleci najprawdopodobniej do countera bo już jest to martwy 
 
     }
 
-    getFormatedTime(date) {
 
-
-        /* const date = getDate(); */
-
-        const year = date.getFullYear();
-        const day = date.getDate();
-        const hours = date.getHours();
-        const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
-
-        return [hours, minutes, seconds]
-
-    }
-    /*   getEndaDate() {
-
-          const day = date.getDate();
-          const year = date.getFullYear();
-          const month = date.getMonth() + 1;
-          return `${year}-${month}-${day} `
-      } */
 
 
     getActualTimeInMilliseconds() {
@@ -104,8 +71,5 @@ class Time { // to wyleci najprawdopodobniej do countera bo już jest to martwy 
     display(unit) {
 
         return unit <= 9 ? `0${unit}` : unit;
-
-
-
     }
 }
