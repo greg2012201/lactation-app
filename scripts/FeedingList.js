af020d0f85e75aae672821c83a83921866c663b0
Grzegorz Dubiel <greg2012201@gmail.com>
2020-11-02 17:40:24 +0100

    FeedingList.js createBefore function inside createLi method created addLi updated

--- scripts/FeedingList.js
+++ scripts/FeedingList.js
@@ -9,59 +9,62 @@ class FeedingList {
     addLi() {
 
         let li;
-        this.li = document.createElement('li');
-        const text = document.createElement('span');
 
         /*  this.li.textContent = `${this.time.getLocalTime().hours}:${this.time.getLocalTime().minutes}:${this.time.getLocalTime().seconds}`; */ // zastanowić się czy nie zrezygnować z metody getLocalTime ? 
         /*  this.ul.appendChild(this.li);
          this.li = this.ul.insertBefore(text, this.li) */
-        this.createLi(li, this.ul);
+
+        this.ul.appendChild(this.createLi())
 
     }
 
-    createLi(li, ul) {
+    createLi() {
 
-        li = document.createElement('li');
+        const li = document.createElement('li');
 
+
+        const createBefore = (parent, elementAfter) => {
+
+            const span = document.createElement('span');
+            span.textContent = ':'
+            parent.insertBefore(span, elementAfter);
+            return span
+        }
 
 
         for (let i = 0; i < 3; i++) {
 
 
             const span = document.createElement('span');
-            if (i == 0) span.className = 'hours';
-            else if (i == 1) span.className = 'minutes';
-            else span.className = 'seconds';
             li.appendChild(span);
-            console.log(li);
+            if (i == 0) span.className = 'hours';
+            else if (i == 1) {
+                span.className = 'minutes';
+                createBefore(li, span);
 
-
-
+            } else {
+                span.className = 'seconds';
+                createBefore(li, span);
+            }
         }
 
-
-        li.childNodes.forEach(element => {
-
-            /* element.className == 'minutes' ? element = li.insertBefore(text, element) : element.className == 'seconds' ? element = li.insertBefore(text, element) : null; */
-            console.log(element);
-            const text = document.createElement('span');
-            text.textContent = ':'
-
-
-
-            if (element.className == 'minutes') element = li.insertBefore(text, element);
-            else
-            if (element.className == 'seconds') element = li.insertBefore(text, element);
-            else return
-
-
-
-        })
-
-        console.log(li);
+        return li;
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     clearList() {
 
